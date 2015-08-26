361510fa8c5930e91a5939a6b9fb0220b3f6aa2c
Mageling <nksadow@gmail.com>
2015-08-25 23:56:34 -0600

    properly add mod skeleton

--- src/mageling/bakery/common/Bakery.java
+++ src/mageling/bakery/common/Bakery.java
@@ -1,5 +1,30 @@
 package mageling.bakery.common;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="bakery", name = "Bakery", version = "0.0")
 public class Bakery {
 
+	@Instance("bakery")
+	public static Bakery instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+
+	}
 }
