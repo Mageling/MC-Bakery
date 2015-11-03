fe4b6f441e602e6f2e7fbd1d1dc477dc55d58cec
Noah Sadow <nsadow@mymail.mines.edu>
2015-11-02 20:53:28 -0700

    Added proxy classes

--- src/mageling/bakery/common/Bakery.java
+++ src/mageling/bakery/common/Bakery.java
@@ -1,8 +1,10 @@
 package mageling.bakery.common;
 
+import mageling.bakery.common.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -10,21 +12,26 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid="bakery", name = "Bakery", version = "0.0")
 public class Bakery {
 
+	
+	
 	@Instance("bakery")
 	public static Bakery instance;
 	
+	@SidedProxy(clientSide = "mageling.bakery.common.proxy.ClientProxy", serverSide = "mageling.bakery.common.proxy.ServerProxy")
+	public static CommonProxy proxy;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-
+		proxy.preInit(event);
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-
+		proxy.init(event);
 	}
 
 	@EventHandler
 	public static void postInit(FMLPostInitializationEvent event) {
-
+		proxy.postInit(event);
 	}
 }
